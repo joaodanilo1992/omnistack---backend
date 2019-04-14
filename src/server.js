681e238a6a14204a5131927c4db592f1756683d4
João Danilo <joaodanilo1992@gmail.com>
2019-04-14 12:10:20 -0400

    Fix dependencias

--- src/server.js
+++ src/server.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 const path = require('path');
 const cors = require('cors');
 
-
 const app = express();
 
 app.use(cors())
 
 const server = require('http').Server(app);
+
 const io = require('socket.io')(server);
 
 io.on('connection', socket => {
